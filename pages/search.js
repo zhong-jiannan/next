55dd8b5e08adbab120ad6f9fb361a1a3a498934f
zhongjiannan <zhongjiannan@outlook.com>
2020-05-31 23:37:18 +0800

    add pagination

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,11 @@
-import { Row, Col, List, Empty } from 'antd'
+import { isValidElement } from 'react'
+import Link from 'next/link'
+import { withRouter } from 'next/router'
+import { Row, Col, List, Empty, Pagination } from 'antd'
 import { request } from '../lib/request'
 import Repo from '../components/repo'
 
-const languages = ['Javascript','HTML','CSS','Java','Rust']
+const languages = ['Javascript','HTML','CSS','TypeScript','Dart']
 
 const sort = [
     {name:'Best Match'},
@@ -12,8 +15,27 @@ const sort = [
     {name:'Fewest Forks',sort:'forks',order:'asc'}
 ]
 
+const noop = ()=>{}
 
-const Search = ({repos})=>{
+const FilterLink = ({query,name,language,sort,order,page}) => {
+
+    let queryString = `/search?query=${query}`
+        if(language) queryString += `&language=${language}`
+        if(sort) queryString += `&sort=${sort}&order=${ order || 'asc'}`
+        if(page) queryString += `&page=${page}`  
+
+    return (
+    <Link href={queryString}>
+        {isValidElement(name) ? name : <a>{name}</a> }
+    </Link>
+    )
+}
+
+const Search = ({repos,router})=>{
+    const total = repos.total_count
+    const query = router.query
+
+    const {page,language} = router.query
 
     return <div className="search-container">
         <Row gutter={20}>
@@ -23,21 +45,43 @@ const Search = ({repos})=>{
                         header={<span className="title">语言</span>}
                         bordered
                         dataSource={languages}
-                        renderItem={ item => <List.Item><a>{item}</a></List.Item> }
+                        renderItem={ item => (
+                        <List.Item>
+                            <FilterLink {...query} name={item} language={item} />
+                        </List.Item>
+                        )}
                         style={{marginBottom:20}}
-                        size="small"
                     />
                     <List 
                         header={<span className="title">排序</span>}
                         bordered
                         dataSource={ sort }
-                        renderItem={ item => <List.Item><a>{item.name}</a></List.Item> }
-                        size="small"
+                        renderItem={ item => (
+                        <List.Item>
+                            <FilterLink {...query} name={item.name} sort={item.sort} order={item.order} />
+                        </List.Item>)}
                     />
                 </div>
             </Col>
             <Col span={19}>
+                <h1 className="result-title">共有<label>{ total }</label>个仓库</h1>
                 { repos.items ? repos.items.map( item => <Repo repo={ item } key={item.id} />) : <Empty /> }
+                <div className="pagination">
+                    <Pagination
+                    showSizeChanger={false}
+                    current={Number(page) || 1}
+                    total={ total < 1000 ? total : 1000 }
+                    pageSize={30}
+                    onChange={noop}
+                    itemRender ={ (page,type,originalElement) => (
+                        <FilterLink
+                        {...query}
+                        name={type === 'page' ? page : originalElement}
+                        page={page} 
+                        />
+                    )}
+                    />
+                </div>
             </Col>
         </Row>
         <style jsx>{`
@@ -52,6 +96,12 @@ const Search = ({repos})=>{
             .stick{
                 position:sticky;
                 top:20px;
+            }
+            .pagination{
+                text-align:center;
+            }
+            .result-title label{
+                color:#1890ff;
             }
         `}</style>
     </div>
@@ -83,4 +133,4 @@ Search.getInitialProps = async ({ctx}) =>{
     }
 }
 
-export default Search
+export default withRouter(Search)
